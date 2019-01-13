3c0fbe88b62f6ed0d2c8175993a81a2aab4c9e46
kishor gujar <kishorgujar95@gmail.com>
2019-01-13 17:20:18 +0530

    v3

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -7,15 +7,15 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
+        <link rel="stylesheet" href="{{ $page->baseUrl }}/{{ mix('css/main.css', 'assets/build') }}">
     </head>
     <body>
         <div class="topnav" id="navbar">
-        <a href="/"{!! $activeNav == '/' ? ' class="active"' : '' !!}> First Generation</a>
-        <a href="/second-generation"{!! $activeNav == 'second-generation' ? ' class="active"' : '' !!}>Second Generation</a>
-        <a href="/third-generation"{!! $activeNav == 'third-generation' ? ' class="active"' : '' !!}>Third Generation</a>
-        <a href="/fourth-generation"{!! $activeNav == 'fourth-generation' ? ' class="active"' : '' !!}>Fourth Generation</a>
-        <a href="/fifth-generation"{!! $activeNav == 'fifth-generation' ? ' class="active"' : '' !!}>Fifth Generation</a>
+        <a href="{{ $page->baseUrl }}/"{!! $activeNav == '/' ? ' class="active"' : '' !!}> First Generation</a>
+        <a href="{{ $page->baseUrl }}/second-generation"{!! $activeNav == 'second-generation' ? ' class="active"' : '' !!}>Second Generation</a>
+        <a href="{{ $page->baseUrl }}/third-generation"{!! $activeNav == 'third-generation' ? ' class="active"' : '' !!}>Third Generation</a>
+        <a href="{{ $page->baseUrl }}/fourth-generation"{!! $activeNav == 'fourth-generation' ? ' class="active"' : '' !!}>Fourth Generation</a>
+        <a href="{{ $page->baseUrl }}/fifth-generation"{!! $activeNav == 'fifth-generation' ? ' class="active"' : '' !!}>Fifth Generation</a>
         <a href="javascript:void(0);" class="icon" onclick="myFunction()">
             <i class="fa fa-bars"></i>
         </a>
